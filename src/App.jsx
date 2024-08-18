dd9e053723c8e5278f9007792b98e91cf299b036
Aya Abdeen <mohamedabdeen898@gmail.com>
2024-08-18 15:58:47 -0500

    Hooked Match and Profile API and handle swipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import { User, MessageCircle, X, Heart } from 'lucide-react'
 import './App.css'
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
+
+const fetchRandomProfile = async () => {
+  const response = await fetch("http://localhost:8080/profiles/random");
+  if(!response.ok){
+    throw new Error ("Failed to fetch profile.");
+  }
+  return response.json();
+}
+
+const match = async (profileId) =>{
+  const response = await fetch("http://localhost:8080/match", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ profileId })
+  });
+  if(!response.ok){
+    throw new Error ( 'Failed to match with profile id = ' + { profileId } );
+  }
+}
 
 function App() {
 
   const [currentScreen, setCurrentScreen] = useState('profile');
   const [input, setInput] = useState('');
+  const [currentProfile, setcurrentProfile] = useState(null);
+
+  useEffect(() => {
+    loadRandomProfile();
+  }, {});
+
+  const loadRandomProfile = async () => {
+    try {
+      const profile = await fetchRandomProfile();
+      setcurrentProfile(profile);
+    }catch (error) {
+      console.error(error);
+    }
+  }
 
   const renderScreen = () => {
     switch (currentScreen) {
       case 'profile':
-        return <ProfileSelector/>;
+        return <ProfileSelector profile={currentProfile} onSwipe={onSwipe} />;
       case 'matches':
         return <MatchesList onSelectMatch={ () => setCurrentScreen("chat") }/>;
       case 'chat':
@@ -26,29 +61,32 @@ function App() {
     }
   }
 
-  const ProfileSelector = () => (
-    <div className="rounded-lg overflow-hidden bg-white shadow-lg">
-      <div className="relative">
-        <img src="http://127.0.0.1:8081/608e55f4-3531-485f-b7ce-8fb158b1c9e4.jpg"/>
-        <div className="absolute bottom-0 left-0 right-0 text-white p-4 bg-gradient-to-t from-black">
-          <h2 className='text-3xl font-bold'>Lina Kim, 31</h2>
+  const ProfileSelector = ({ profile, onSwipe }) => (
+    profile ? 
+    (
+      <div className="rounded-lg overflow-hidden bg-white shadow-lg">
+        <div className="relative">
+          <img src={ 'http://127.0.0.1:8081/' + profile.imageUrl }/>
+          <div className="absolute bottom-0 left-0 right-0 text-white p-4 bg-gradient-to-t from-black">
+            <h2 className='text-3xl font-bold'>{profile.firstName} {profile.lastName}, {profile.age}</h2>
+          </div>
         </div>
+        <div className="p-4">
+          <p className="text-gray-600 mb-4">{profile.bio}</p>
+        </div>
+        <div className="p-4 flex justify-center space-x-4">
+          <button className="bg-red-500 rounded-full p-4 text-white hover:bg-red-700"
+            onClick={() => onSwipe( profile.id, "pass" )}>
+            <X size={24}/>
+          </button>
+          <button className="bg-green-500 rounded-full p-4 text-white hover:bg-green-700"
+            onClick={() => onSwipe( profile.id, "like" )}>
+            <Heart size={24}/>
+          </button>
+        </div>
+    
       </div>
-      <div className="p-4">
-        <p className="text-gray-600 mb-4">Architect and designer. Passionate about creating spaces that inspire. Looking for someone who appreciates creativity and innovation.</p>
-      </div>
-      <div className="p-4 flex justify-center space-x-4">
-        <button className="bg-red-500 rounded-full p-4 text-white hover:bg-red-700"
-          onClick={() => console.log("left")}>
-          <X size={24}/>
-        </button>
-        <button className="bg-green-500 rounded-full p-4 text-white hover:bg-green-700"
-          onClick={() => console.log("right")}>
-          <Heart size={24}/>
-        </button>
-      </div>
-  
-    </div>
+    ) : (<div>Loading...</div>)
   );
 
   const MatchesList = ({ onSelectMatch }) => (
@@ -116,6 +154,13 @@ function App() {
       </div>
     </div>
   );
+
+  const onSwipe = (profileId, direction) => {
+    if(direction === 'like' && profileId){
+      match(profileId);
+    }
+    loadRandomProfile();
+  };
 
   return (
     <>
