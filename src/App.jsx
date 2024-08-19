540966588e32732e96299d8515fb2dcb3856878f
Aya Abdeen <mohamedabdeen898@gmail.com>
2024-08-18 19:21:39 -0500

    Send Messages to a match profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { User, MessageCircle, X, Heart } from 'lucide-react'
+import { User, MessageCircle, X, Heart, Send } from 'lucide-react'
 import './App.css'
 import react, { useState, useEffect } from 'react';
 
@@ -10,6 +10,7 @@ const fetchRandomProfile = async () => {
   return response.json();
 }
 
+// saveSwipe
 const match = async (profileId) => {
   const response = await fetch("http://localhost:8080/match", {
     method: 'POST',
@@ -31,12 +32,127 @@ const fetchMatches = async () => {
   return response.json();
 }
 
+const fetchConversationById = async (conversationId) => {
+  const response = await fetch(`http://localhost:8080/conversations/${conversationId}`);
+  if(!response.ok){
+    throw new Error ("Failed to fetch Conversation By Id = " + { conversationId });
+  }
+  return response.json();
+}
+
+const sendMessage = async (conversationId, message) => {
+  const response = await fetch(`http://localhost:8080/conversations/${conversationId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ messageText: message, authorId: 'user' })
+  });
+  if(!response.ok){
+    throw new Error ('Failed to send a message Conversation By Id = ' + { conversationId });
+  }
+}
+
+const ProfileSelector = ({ profile, onSwipe }) => (
+  profile ? 
+  (
+    <div className="rounded-lg overflow-hidden bg-white shadow-lg">
+      <div className="relative">
+        <img src={ 'http://127.0.0.1:8081/' + profile.imageUrl }/>
+        <div className="absolute bottom-0 left-0 right-0 text-white p-4 bg-gradient-to-t from-black">
+          <h2 className='text-3xl font-bold'>{profile.firstName} {profile.lastName}, {profile.age}</h2>
+        </div>
+      </div>
+      <div className="p-4">
+        <p className="text-gray-600 mb-4">{profile.bio}</p>
+      </div>
+      <div className="p-4 flex justify-center space-x-4">
+        <button className="bg-red-500 rounded-full p-4 text-white hover:bg-red-700"
+          onClick={() => onSwipe( profile.id, "pass" )}>
+          <X size={24}/>
+        </button>
+        <button className="bg-green-500 rounded-full p-4 text-white hover:bg-green-700"
+          onClick={() => onSwipe( profile.id, "like" )}>
+          <Heart size={24}/>
+        </button>
+      </div>
+  
+    </div>
+  ) : (<div>Loading...</div>)
+);
+
+const MatchesList = ({ matches, onSelectMatch }) => 
+{
+  return (
+    <div className="rounded-lg shadow-lg p-4">
+    <h2 className="text-2x1 font-bold mb-4">Matches</h2>
+    <ul>
+      {
+        matches.map(match => (
+          <li key={match.profile.id} className="mb-2">
+            <button className="w-full hover:bg-gray-100 rounded flex item-center" onClick={ () => onSelectMatch(match.profile, match.conversationId) }>
+              <img src={ 'http://127.0.0.1:8081/' + match.profile.imageUrl } className="w-16 h-16 rounded-full mr-3 object-cover"/>
+              <span>
+                <h3 className="font-bold">{match.profile.firstName} {match.profile.lastName}</h3>
+              </span>
+            </button>
+          </li>
+        ))
+      }
+   </ul>
+  </div>
+  );
+}
+
+const ChatScreen = ({ currentMatch, conversation, refreshState }) => {
+  const [input, setInput] = useState('');
+
+  const handleSend = async (conversation, input) => {
+    if (input.trim()) {
+      await sendMessage(conversation.id, input.trim());
+      setInput('');
+    }
+    refreshState();
+  }
+
+  return currentMatch ? 
+  (
+    <div className="rounded-lg shadow-lg p-4">
+      <h2 className="text-2xl font-bold mb-4">Chat with {currentMatch.firstName} {currentMatch.lastName}</h2>
+      <div className="h-[50vh] border rounded overflow-y-auto mb-4 p-4">
+        {
+          conversation.messages.map((message, index) => (
+            <dev key={index}>
+              <div className="mb-4 p-2 rounded bg-gray-100">{message.messageText}</div>
+            </dev>
+          ))
+        }
+      </div>
+      <div className="flex">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="flex-1 border-2 border-gray-300 rounded-full py-2 px-4 mr-2 focus:outline-none focus:border-blue-500"
+          placeholder="Type a message..."
+        />
+        <button 
+          className="bg-blue-500 text-white rounded p-2"
+          onClick={() => handleSend(conversation, input)}
+        >
+          <Send size={24} />
+        </button>
+      </div>
+    </div>
+  ) : (<div>Loading...</div>);
+}
+
 function App() {
 
   const [currentScreen, setCurrentScreen] = useState('profile');
-  const [input, setInput] = useState('');
   const [currentProfile, setcurrentProfile] = useState(null);
   const [matches, setMatches] = useState([]);
+  const [currentMatchAndConversation, setCurrentMatchAndConversation] = useState({ match: {}, conversation: [] });
 
   useEffect(() => {
     loadRandomProfile();
@@ -66,100 +182,19 @@ function App() {
       case 'profile':
         return <ProfileSelector profile={currentProfile} onSwipe={onSwipe} />;
       case 'matches':
-        return <MatchesList matches={matches} onSelectMatch={ () => setCurrentScreen("chat") }/>;
+        return <MatchesList matches={matches} onSelectMatch={ onSelectMatch }/>;
       case 'chat':
-        return <ChatScreen/>;
-
+        return <ChatScreen 
+              currentMatch={currentMatchAndConversation.match} 
+        conversation={currentMatchAndConversation.conversation}  
+        refreshState={refreshChatState} />;
     }
   }
 
-  const handleSend = () => {
-    if(input.trim()){
-      console.log(input.trim());
-      setInput('');
-    }
+  const refreshChatState = async () => {
+    const conversation = await fetchConversationById(currentMatchAndConversation.conversation.id);
+    setCurrentMatchAndConversation({match: currentMatchAndConversation.match, conversation: conversation});
   }
-
-  const ProfileSelector = ({ profile, onSwipe }) => (
-    profile ? 
-    (
-      <div className="rounded-lg overflow-hidden bg-white shadow-lg">
-        <div className="relative">
-          <img src={ 'http://127.0.0.1:8081/' + profile.imageUrl }/>
-          <div className="absolute bottom-0 left-0 right-0 text-white p-4 bg-gradient-to-t from-black">
-            <h2 className='text-3xl font-bold'>{profile.firstName} {profile.lastName}, {profile.age}</h2>
-          </div>
-        </div>
-        <div className="p-4">
-          <p className="text-gray-600 mb-4">{profile.bio}</p>
-        </div>
-        <div className="p-4 flex justify-center space-x-4">
-          <button className="bg-red-500 rounded-full p-4 text-white hover:bg-red-700"
-            onClick={() => onSwipe( profile.id, "pass" )}>
-            <X size={24}/>
-          </button>
-          <button className="bg-green-500 rounded-full p-4 text-white hover:bg-green-700"
-            onClick={() => onSwipe( profile.id, "like" )}>
-            <Heart size={24}/>
-          </button>
-        </div>
-    
-      </div>
-    ) : (<div>Loading...</div>)
-  );
-
-  const MatchesList = ({ matches, onSelectMatch }) => (
-    <div className="rounded-lg shadow-lg p-4">
-      <h2 className="text-2x1 font-bold mb-4">Matches</h2>
-      <ul>
-        {
-          matches.map(match => (
-            <li key={match.profile.id} className="mb-2">
-              <button className="w-full hover:bg-gray-100 rounded flex item-center" onClick={ onSelectMatch }>
-                <img src={ 'http://127.0.0.1:8081/' + match.profile.imageUrl } className="w-16 h-16 rounded-full mr-3 object-cover"/>
-                <span>
-                  <h3 className="font-bold">{match.profile.firstName} {match.profile.lastName}</h3>
-                </span>
-              </button>
-            </li>
-          ))
-        }
-     </ul>
-    </div>    
-  );
-  
-  const ChatScreen = () => (
-    <div className="rounded-lg shadow-lg p-4">
-      <h2 className="text-2xl font-bold mb-4">Chat with Lina Kim</h2>
-      <div className="h-[50vh] border rounded overflow-y-auto mb-4 p-4">
-        {
-          [
-            "Hi",
-            "How are you ?"
-          ].map((messege, index) => (
-            <dev key={index}>
-              <div className="mb-4 p-2 rounded bg-gray-100">{messege}</div>
-            </dev>
-          ))
-        }
-      </div>
-      <div className="flex">
-        <input 
-          type="text" 
-          className="border flex-1 rounded p-2 mr-2" 
-          placeholder="Type a message..."
-          value={input}
-          onChange={ (e) => setInput(e.target.value) }
-        />
-        <button 
-          className="bg-blue-500 text-white rounded p-2"
-          onClick={ handleSend }
-          >
-          Send
-        </button>
-      </div>
-    </div>
-  );
 
   const onSwipe = async (profileId, direction) => {
     loadRandomProfile();
@@ -168,6 +203,12 @@ function App() {
       await loadMatches()
     }
   };
+
+  const onSelectMatch = async (profile, conversationId) => {
+    const conversation = await fetchConversationById(conversationId);
+    setCurrentMatchAndConversation({ match: profile, conversation: conversation});
+    setCurrentScreen('chat');
+  }
 
   return (
     <>
